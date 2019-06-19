607e5ba842176b0eec9bf41eb67406bf001957f2
arimbr <arimbr@gmail.com>
2019-06-19 19:01:06 +0200

    Use single quotes

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="airflow-valohai-plugin",
-    version="0.0.1",
+    name='airflow-valohai-plugin',
+    version='0.0.1',
     description='Airflow plugin to launch Valohai executions from Airflow tasks',
     author='Skillup',
     author_email='ari@skillup.co',
     license='MIT',
     packages=find_packages(include=('airflow_valohai_plugin*',)),
     entry_points={
-        "airflow.plugins": [
-            "valohai_plugin = airflow_valohai_plugin.valohai_plugin:ValohaiPlugin"
+        'airflow.plugins': [
+            'valohai_plugin = airflow_valohai_plugin.valohai_plugin:ValohaiPlugin'
         ]
     }
 )
