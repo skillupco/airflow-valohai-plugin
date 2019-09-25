b92219a9158ea05d78b8a29eb507e718cc32357a
arimbr <arimbr@gmail.com>
2019-09-25 15:54:08 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='airflow-valohai-plugin',
-    version='0.0.1',
+    version='0.0.2',
     description='Airflow plugin to launch Valohai executions from Airflow tasks',
     author='Skillup',
     author_email='ari@skillup.co',
