d0d839462c937ba088012a95777b1ce9bab3a36f
arimbr <arimbr@gmail.com>
2019-10-01 18:33:08 +0200

    By default download all outputs

--- airflow_valohai_plugin/operators/valohai_download_execution_outputs_operator.py
+++ airflow_valohai_plugin/operators/valohai_download_execution_outputs_operator.py
@@ -9,17 +9,17 @@ from airflow.configuration import AIRFLOW_HOME
 
 class ValohaiDownloadExecutionOutputsOperator(BaseOperator):
     """
-    Downloads outputs locally from a ValohaiSubmitExecutionOperator
-    tasks that stored outputs details in XCOM.
+    Downloads outputs locally from a previous ValohaiSubmitExecutionOperator
+    task that stored output details in XCOM. By default it downloads all outputs.
 
     Args:
         output_task (TaskInstance): instance of a ValohaiSubmitExecutionOperator
             task that produced the outputs.
-        output_name (str, optional): filter output name to donwload.
+        output_name (str, optional): filter output name to download.
         output_name_pattern (str, optional): filter output name to download
             with a regex.
         output_path (str, optional): relative path to AIRFLOW_HOME where to
-            store the outputs locally.
+            store the outputs locally. By default stores ouputs in AIRFLOW_HOME.
     """
     ui_color = '#fff'
     ui_fgcolor = '#000'
@@ -50,20 +50,22 @@ class ValohaiDownloadExecutionOutputsOperator(BaseOperator):
             include_prior_dates=True)
 
         for output in execution_details['outputs']:
-            if self.output_name and not self.output_name == output['name']:
-                logging.info('Ignore ouput name {}'.format(
-                    output['name']))
-                continue
-            else:
-                output_path = self.get_output_path(self.output_name)
-
-            if self.output_name_pattern:
+            if self.output_name:
+                if not self.output_name == output['name']:
+                    logging.info('Ignore ouput name {}'.format(
+                        output['name']))
+                    continue
+                output_name = self.output_name
+            elif self.output_name_pattern:
                 name_match = re.match(self.output_name_pattern, output['name'])
                 if not name_match:
                     logging.info('Ignore ouput name {} because failed to match pattern {}'.format(
                         output['name'], self.output_name_pattern))
                     continue
-                output_path = self.get_output_path(name_match.group(0))
+                output_name = name_match.group(0)
+            else:
+                output_name = output['name']
 
+            output_path = self.get_output_path(output_name)
             urlretrieve(output['url'], output_path)
-            logging.info('Downloaded output to: {}'.format(output_path))
+            logging.info('Downloaded output {} to: {}'.format(output_name, output_path))
